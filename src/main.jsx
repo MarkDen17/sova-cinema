5de48ec78b332c2c4ef8900d58de7b17f8b8c100
MarkDen17 <markden17@mail.ru>
2025-04-20 18:37:38 +0300

    refacored imports

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
+import '@/assets/styles/index.css'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import '@/assets/styles/index.css'
-import App from './App.jsx'
-import { store } from './store/store.js'
 import { Provider } from 'react-redux'
+import App from './App.jsx'
+import './index.css'
+import { store } from './store/store.js'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
